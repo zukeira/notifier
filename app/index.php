456b3a56015a0e6bfd18d449201b9a57c2ced24a
zukeira <147625754+zukeira@users.noreply.github.com>
2023-10-18 20:42:11 -0300

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,12 +1,47 @@
 <?php include "../config.php"; ?>
 <html>
+<head>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      text-align: center;
+      background-color: #f0f0f0;
+    }
+    h1 {
+      color: #333;
+    }
+    table {
+      border-collapse: collapse;
+      margin: 0 auto;
+    }
+    table, th, td {
+      border: 2px solid #333;
+    }
+    th, td {
+      padding: 10px;
+    }
+    th {
+      background-color: #333;
+      color: #fff;
+    }
+    input[type="text"] {
+      padding: 5px;
+      width: 100%;
+      box-sizing: border-box;
+    }
+    input[type="submit"] {
+      background-color: #333;
+      color: #fff;
+      padding: 10px 20px;
+      border: none;
+      cursor: pointer;
+    }
+  </style>
+</head>
 <body>
-  <font face="Arial">
-    <h1>Bem vindo a Renova</h1>
-  </font>
+  <h1>Bem vindo a Renova</h1>
 
-<?php
-
+  <?php
   /* Connect to MySQL and select the database. */
   $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
 
@@ -24,99 +59,82 @@
   if (strlen($user_name) || strlen($user_cellphone)) {
     AddUser($connection, $user_name, $user_cellphone);
   }
-?>
+  ?>
 
-<!-- Input form -->
-<form action="<?PHP echo $_SERVER['SCRIPT_NAME'] ?>" method="POST">
-  <font face="Arial" size="10">
-    <table border="0">
+  <!-- Input form -->
+  <form action="<?PHP echo $_SERVER['SCRIPT_NAME'] ?>" method="POST">
+    <table>
       <tr>
         <td><b>Name</b></td>
         <td><b>Cellphone</b></td>
       </tr>
       <tr>
         <td>
-          <input type="text" name="NAME" maxlength="45" size="30" />
+          <input type="text" name="NAME" maxlength="45" />
         </td>
         <td>
-          <input type="text" name="CELLPHONE" maxlength="90" size="15" />
+          <input type="text" name="CELLPHONE" maxlength="90" />
         </td>
       </tr>
       <tr>
-        <td>
+        <td colspan="2">
           <input type="submit" value="Add" />
         </td>
       </tr>
     </table>
-  </font>
-</form>
+  </form>
 
-<!-- Display table data. -->
-<br>
-<font face="Arial">
+  <!-- Display table data -->
+  <br>
   <h1>Contact List</h1>
-</font>
-<font face="Arial" size="10">
-  <table border="2" cellpadding="2" cellspacing="2" width="380px">
+  <table>
     <tr>
-      <td align="center" width="50px"><b>id</b></td>
-      <td align="center" width="200px"><b>Name</b></td>
-      <td align="center" width="130px"><b>Cellphone</b></td>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Cellphone</th>
     </tr>
+    <?php
+    $result = mysqli_query($connection, "SELECT * FROM USERS");
 
-<?php
-
-$result = mysqli_query($connection, "SELECT * FROM USERS");
-
-while($query_data = mysqli_fetch_row($result)) {
-  echo "<tr>";
-  echo "<td align=\"center\">",$query_data[0], "</td>",
-       "<td>",$query_data[1], "</td>",
-       "<td>",$query_data[2], "</td>";
-  echo "</tr>";
-}
-?>
-
+    while ($query_data = mysqli_fetch_row($result)) {
+      echo "<tr>";
+      echo "<td align=\"center\">", $query_data[0], "</td>",
+      "<td>", $query_data[1], "</td>",
+      "<td>", $query_data[2], "</td>";
+      echo "</tr>";
+    }
+    ?>
   </table>
-</font>
 
-<!-- Clean up. -->
-<?php
-
+  <!-- Clean up. -->
+  <?php
   mysqli_free_result($result);
   mysqli_close($connection);
-
-?>
-
-
+  ?>
 
 </body>
 </html>
-
-
 <?php
-
-/* Add an User to the table. */
+/* Add a User to the table. */
 function AddUser($connection, $name, $cellphone) {
-   $n = mysqli_real_escape_string($connection, $name);
-   $c = mysqli_real_escape_string($connection, $cellphone);
+  $n = mysqli_real_escape_string($connection, $name);
+  $c = mysqli_real_escape_string($connection, $cellphone);
 
-   $query = "INSERT INTO USERS (NAME, CELLPHONE) VALUES ('$n', '$c');";
+  $query = "INSERT INTO USERS (NAME, CELLPHONE) VALUES ('$n', '$c');";
 
-   if(!mysqli_query($connection, $query)) echo("<p>Error adding employee data.</p>");
+  if (!mysqli_query($connection, $query)) echo ("<p>Error adding employee data.</p>");
 }
 
 /* Check whether the table exists and, if not, create it. */
 function VerifyUsersTable($connection, $dbName) {
-  if(!TableExists("USERS", $connection, $dbName))
-  {
-     $query = "CREATE TABLE USERS (
+  if (!TableExists("USERS", $connection, $dbName)) {
+    $query = "CREATE TABLE USERS (
          ID int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
          NAME VARCHAR(45),
          CELLPHONE VARCHAR(90)
        )";
 
-     if(!mysqli_query($connection, $query)) echo("<p>Error creating table.</p>");
+    if (!mysqli_query($connection, $query)) echo ("<p>Error creating table.</p>");
   }
 }
 
@@ -126,9 +144,9 @@ function TableExists($tableName, $connection, $dbName) {
   $d = mysqli_real_escape_string($connection, $dbName);
 
   $checktable = mysqli_query($connection,
-      "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = '$t' AND TABLE_SCHEMA = '$d'");
+    "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = '$t' AND TABLE_SCHEMA = '$d'");
 
-  if(mysqli_num_rows($checktable) > 0) return true;
+  if (mysqli_num_rows($checktable) > 0) return true;
 
   return false;
 }
