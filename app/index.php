bd2d948b2ae94aa9247180cfd492855b550d2291
unknown <perfil@ESTUDIO11.fiap.com.br>
2023-10-19 18:17:35 -0300

    testando o index

--- app/index.php
+++ app/index.php
@@ -84,27 +84,7 @@
     </table>
   </form>
 
-  <!-- Display table data -->
-  <br>
-  <h1>Contact List</h1>
-  <table>
-    <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Cellphone</th>
-    </tr>
-    <?php
-    $result = mysqli_query($connection, "SELECT * FROM USERS");
 
-    while ($query_data = mysqli_fetch_row($result)) {
-      echo "<tr>";
-      echo "<td align=\"center\">", $query_data[0], "</td>",
-      "<td>", $query_data[1], "</td>",
-      "<td>", $query_data[2], "</td>";
-      echo "</tr>";
-    }
-    ?>
-  </table>
 
   <!-- Clean up. -->
   <?php
