43603e1fb55060a6c0372ee42f291e3820563842
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-19 21:05:10 -0300

    code montez

--- app/index.php
+++ app/index.php
@@ -1,16 +1,11 @@
 <?php include "../config.php"; ?>
 <html>
 <head>
-  
   <style>
     body {
-      font-family: Roboto;
-      background-repeat: no-repeat;
-      background-size: cover;
+      font-family: Arial, sans-serif;
       text-align: center;
-      background: linear-gradient(120deg, #ffffff, #00cc76, #73ffca);
-      height: 100vh;
-      overflow: hidden;
+      background-color: #f0f0f0;
     }
     h1 {
       color: #333;
@@ -41,111 +36,78 @@
       border: none;
       cursor: pointer;
     }
-
-    a{
-      width: 100%;
-      height: 50px;
-      border: 1px solid;
-      border-radius: 25px;
-      font-size: 18px;
-      font-weight: 700;
+    a {
+      background-color: #333;
+      color: #fff;
+      padding: 10px 20px;
+      border: none;
       cursor: pointer;
-      background-color: #00cc76;
-      color: #ffffff;
+      text-decoration: none;
+    }
+    .menu {
+      display: flex;
+      justify-content: space-around;
+      background-color: #333;
+      padding: 10px 0;
+    }
+    .menu a:hover {
+      background-color: #444;
     }
   </style>
 </head>
 <body>
   <h1>Bem vindo a Renova</h1>
+  <div class="menu">
+    <a href="#proposta">Proposta</a>
+    <a href="#produto">Produto</a>
+    <a href="#valor">Valor</a>
+    <a href="#cadastro">Cadastro</a>
+  </div>
 
-  <?php
-  /* Connect to MySQL and select the database. */
-  $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+  <!-- Seção de Proposta -->
+  <section id="proposta">
+    <h2>Proposta</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  </section>
 
-  if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  <!-- Seção de Produto -->
+  <section id="produto">
+    <h2>Produto</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  </section>
 
-  $database = mysqli_select_db($connection, DB_DATABASE);
+  <!-- Seção de Valor -->
+  <section id="valor">
+    <h2>Valor</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  </section>
 
-  /* Ensure that the USERS table exists. */
-  VerifyUsersTable($connection, DB_DATABASE);
+  <!-- Seção de Cadastro -->
+  <section id="cadastro">
+    <h2>Cadastro</h2>
+    <form action="<?PHP echo $_SERVER['SCRIPT_NAME'] ?>" method="POST">
+      <table>
+        <tr>
+          <td><b>Nome</b></td>
+          <td><b>CPF</b></td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" name="NAME" maxlength="45" />
+          </td>
+          <td>
+            <input type="text" name="CELLPHONE" maxlength="90" />
+          </td>
+        </tr>
+        <tr>
+          <td colspan="2">
+            <input type="submit" value="Cadastrar" />
+          </td>
+        </tr>
+      </table>
+    </form>
+  </section>
 
-  /* If input fields are populated, add a row to the USERS table. */
-  $user_name = htmlentities($_POST['NAME']);
-  $user_cellphone = htmlentities($_POST['CELLPHONE']);
-
-  if (strlen($user_name) || strlen($user_cellphone)) {
-    AddUser($connection, $user_name, $user_cellphone);
-  }
-  ?>
-
-  <!-- Input form -->
-  <form action="<?PHP echo $_SERVER['SCRIPT_NAME'] ?>" method="POST">
-    <table>
-      <tr>
-        <td><b>Nome</b></td>
-        <td><b>CPF</b></td>
-      </tr>
-      <tr>
-        <td>
-          <input type="text" name="NAME" maxlength="45" />
-        </td>
-        <td>
-          <input type="text" name="CELLPHONE" maxlength="90" />
-        </td>
-      </tr>
-      <tr>
-        <td colspan="2">
-          <input type="submit" value="Add" />
-        </td>
-      </tr>
-    </table>
-  </form>
-
-<a href="adm.php">Administrador</a>
-
-
-  <!-- Clean up. -->
-  <?php
-  mysqli_free_result($result);
-  mysqli_close($connection);
-  ?>
-
+  <a href="adm.php">Administrador</a>
 </body>
 </html>
-<?php
-/* Add a User to the table. */
-function AddUser($connection, $name, $cellphone) {
-  $n = mysqli_real_escape_string($connection, $name);
-  $c = mysqli_real_escape_string($connection, $cellphone);
-
-  $query = "INSERT INTO USERS (NAME, CELLPHONE) VALUES ('$n', '$c');";
-
-  if (!mysqli_query($connection, $query)) echo ("<p>Error adding employee data.</p>");
-}
-
-/* Check whether the table exists and, if not, create it. */
-function VerifyUsersTable($connection, $dbName) {
-  if (!TableExists("USERS", $connection, $dbName)) {
-    $query = "CREATE TABLE USERS (
-         ID int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-         NAME VARCHAR(45),
-         CELLPHONE VARCHAR(90)
-       )";
-
-    if (!mysqli_query($connection, $query)) echo ("<p>Error creating table.</p>");
-  }
-}
-
-/* Check for the existence of a table. */
-function TableExists($tableName, $connection, $dbName) {
-  $t = mysqli_real_escape_string($connection, $tableName);
-  $d = mysqli_real_escape_string($connection, $dbName);
-
-  $checktable = mysqli_query($connection,
-    "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = '$t' AND TABLE_SCHEMA = '$d'");
-
-  if (mysqli_num_rows($checktable) > 0) return true;
-
-  return false;
-}
-?>
