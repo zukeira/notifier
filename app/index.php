b00526dffb3de25fb53994b2b1dde409855ed0a0
unknown <boyczukgameplay2002@gmail.com>
2023-10-21 17:04:05 -0300

    submmit

--- app/index.php
+++ app/index.php
@@ -272,7 +272,7 @@
         <form action="<?PHP echo $_SERVER['SCRIPT_NAME'] ?>" method="POST">
           <table border="0">
             <div class="txt_field">
-              <input type="text" name="name" required>
+              <input type="text" name="NAME" required>
               <span></span>
               <label>Nome</label>
             </div>
@@ -281,8 +281,8 @@
               <span></span>
               <label>CPF</label>
             </div>
-            <input type="submit" value="Cadastrar" />
           </table>
+          <input type="submit" value="Cadastrar" />
         </form>
       </div>
     </div>  
